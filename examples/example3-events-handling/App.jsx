f32d964480af183cc1da259c30c0a1729571cdfd
ian Shen <ianshen@ians-MacBook-Pro.local>
2018-04-20 13:52:23 +0200

    test different events

--- examples/example3-events-handling/App.jsx
+++ examples/example3-events-handling/App.jsx
@@ -4,7 +4,14 @@ class App extends Component {
     constructor(props) {
         super(props);
         //set default state
+        this.state = {
+            toggleName: 'yes',
+        };
         this.handleClick = this.handleClick.bind(this, props.name);
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
+        this.handleCopy = this.handleCopy.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
     }
 
     // handleClick(param, e) {
@@ -15,13 +22,32 @@ class App extends Component {
         console.log('event', e);
         console.log('param', param);
     }
-
+    handleMouseEnter = (e) => {
+        console.log('Mouse entered!');
+    }
+    handleMouseLeave = (e) => {
+        console.log('Mouse left!');
+    }
+    handleCopy = (e) => {
+        console.log(e.clipboardData);
+    }
+    handleToggle = (e) => {
+        console.log('toggle');
+        if (this.state.toggleName === 'yes') {
+            this.setState({toggleName: 'no'})
+        } else {
+            this.setState({toggleName: 'yes'})
+        }
+        
+    }
     render() {
         const props = this.props;
         return(
             <div>
                 <h2>{props.name}</h2>
-                <button onClick={this.handleClick(props.name)}>test</button>
+                <input readOnly type="text" value = {props.name} onCopy = {this.handleCopy}/>
+                <button onClick={this.handleToggle} >{this.state.toggleName}</button>
+                <button onClickCapture={this.handleClick(props.name)} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} >test</button>
             </div>
         );
     }
